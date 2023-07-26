9342576717318386815a1758c159e867ef5d0440
amirticha <allagui_fawzi@outlook.fr>
2023-07-26 16:51:09 +0100

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import './App.css'
 import Navbar from './components/Navbar';
 import { SelectedPage } from './shared/types'
 import { motion, useAnimation } from 'framer-motion';
+import Introduction from './components/Introduction';
 
 
 
@@ -53,6 +54,15 @@ const App = () => {
         markers: true,
       },
     });
+    
+    gsap.to('.txt-bottom',{
+      autoAlpha:0,
+      letterSpacing:-10,
+      duration:2,
+      scrollTrigger:{
+        start:2
+      }
+    })
 
     gsap.to('.right', {
       autoAlpha: 0,
@@ -72,6 +82,36 @@ const App = () => {
       },
     });
   });
+ 
+gsap.utils.toArray('.col').forEach(image=>{
+  gsap.fromTo(image,{
+    opacity:.3,
+    x:0
+  },{
+    opacity:1,
+    x:-50,
+    scrollTrigger:{
+      trigger:image,
+      start:"10%",
+      stagger:{
+        amount:.4
+      }
+    }
+  })
+})
+
+const timeline = gsap.timeline();
+
+timeline.from('.title span' ,{
+  y:150,
+  skewY:7,
+  duration:3
+}).from('.txt-bottom',{
+  letterSpacing:-10,
+  opacity:0,
+  duration:3
+})
+ 
 
 
   return (
@@ -96,7 +136,7 @@ const App = () => {
                                         initial="hidden"
                                         whileInView="visible"
                                         viewport={{ once: true, amount: 0.5 }}
-                                        transition={{ duration: 1 ,delay: 1 }}
+                                        transition={{ duration: 0.7 ,delay: 1 }}
                                         variants={{
                                           hidden: { opacity: 0 ,y:"-10%"},
                                           visible: { opacity: 1,y:"40%"},                           
@@ -113,7 +153,7 @@ const App = () => {
                                         initial="hidden"
                                         whileInView="visible"
                                         viewport={{ once: true, amount: 0.5 }}
-                                        transition={{ duration: 1 ,delay: 2 }}
+                                        transition={{ duration: 0.7 ,delay:1 }}
                                         variants={{
                                           hidden: { opacity: 0 ,y:"100%"},
                                           visible: { opacity: 1,y:"40%"},                           
@@ -128,9 +168,104 @@ const App = () => {
                               </motion.div>
                               
                       </div>
-                      <p className=" font-bold  txt-bottom">websites that will change your mind</p>
+                      <motion.div 
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{ once: true, amount: 0.5 }}
+                                transition={{ duration: 0.7 ,delay:2 }}
+                                variants={{
+                                  hidden: { opacity: 0 ,y:"100%"},
+                                  visible: { opacity: 1,y:"40%"},                           
+                                }}
+                                        >
+                            <p className="txt-bottom font-bold ">websites that will change your mind</p>
+                      </motion.div>
                   </div>
+                  <Introduction />
               </section>
+
+              <section className="section">
+            <div className="boxes-container">
+                 {/* <!-- ************************COLUMN1********************************************* --> */}
+                <div className="columns box1">
+                    <div className="col col-1">
+                        <img src="./media/1.jpeg" alt=""/>
+                        <img src="./media/2.jpeg" alt=""/>
+                    </div>
+                    <div className="col-2">
+                        <h1>Carolina</h1>
+                        <span>28 october 2021</span>
+                        <div className="line"></div>
+                        <div className="text-box">
+                            <p>
+                                This series is excellently showing that ordinary
+                                portrait photography still can be inspiring for you.
+                            </p>
+                            <p>
+                                Check the shots of beautiful Caroline in hat with red and green lights.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col col-3">
+                        <img src="./media/3.webp" alt=""/>
+                        <img src="./media/4.jpeg" alt=""/>
+                        <img src="./media/5.jpeg" alt=""/>
+                    </div>
+                </div>
+                {/* <!-- *******************************COLUMN2************************************** --> */}
+                <div className="columns box2">
+                    <div className="col col-1">
+                        <img src="./media/p1.jpeg" alt=""/>
+                        <img src="./media/p2.webp" alt=""/>
+                    </div>
+                    <div className="col-2">
+                        <h1>soul dance</h1>
+                        <span>17 september 2021</span>
+                        <div className="line"></div>
+                        <div className="text-box">
+                            <p>
+                                Beautiful dance of Hanna in neon ligths with retro effect. Pink lights, pretty women
+                                and sensuality.
+                            </p>
+                            <p>
+                                Inspiring vibes and invisible beautiful soul on this shots.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col col-3">
+                        <img src="./media/p3.jpeg" alt=""/>
+                        <img src="./media/p4.jpeg" alt=""/>
+                        <img src="./media/p5.jpeg" alt=""/>
+                    </div>
+                </div>
+               {/* <!-- *******************************COLUMN3************************************** --> */}
+                <div className="columns box3">
+                    <div className="col col-1">
+                        <img src="./media/b1.jpeg" alt=""/>
+                        <img src="./media/b2.webp" alt=""/>
+                        <img src="./media/b3.jpeg" alt=""/>
+                        <img src="./media/b4.jpeg" alt=""/>
+                    </div>
+                    <div className="col-2">
+                        <span>1 september 2021</span>
+                        <div className="line"></div>
+                        <div className="text-box">
+                            <p>
+                                Retro nostagia can be sweet and sour at the same time. Christina helped us to make
+                                photos, that will give you this feelings.
+
+                            </p>
+                            <p>
+
+                            </p>
+                        </div>
+                    </div>
+                    <div className="col col-3">
+                        <img src="./media/b5.webp" alt=""/>
+                    </div>
+                </div>
+            </div>
+        </section>
             </div>
         <div className="text-white text-4xl font-bold">Welcome to My Website</div>
       </div>
---
+++ src/components/Introduction.jsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+function Introduction() {
+
+    useEffect(() => {
+
+        ScrollTrigger.defaults({
+          toggleActions:"play none none reverse"
+        })
+
+        const tl = gsap.timeline();
+
+            tl.from('.left-side div',{
+            y:150,
+            opacity:0,
+            stagger:{
+                amount:.4
+            },
+            delay:.5
+            }).from('.right-side',{opacity:0,duration:2},.5)
+            .to('.wrapper' ,{x:-window.innerWidth})
+
+
+        
+ScrollTrigger.create({
+  animation:tl,
+  trigger:'.wrapper',
+  start:"top top",
+  end:"+=600",
+  scrub:1,
+  pin:true,
+  ease:"ease"
+})
+
+
+    });
+  return (
+    <div>
+        <div className="v_container">
+                <div className="left-side">
+                    <div className="tv">
+                        <div className="bg">Hi,</div>
+                        <div className="sm">I m </div>
+                        <div className="bg bottom">AMIRA</div>
+                    </div>
+                    <div className="text-container">
+                        <p>
+                        From front-end development to seamless back-end integration, I am devoted to creating exceptional online experiences that captivate audiences and drive business growth
+
+                        </p>
+                        <p>
+                        It has become widespread and diverse, but as developers, we recognize that it is still a true form of skill and innovation.
+
+                        </p>
+                    </div>
+                </div>
+                <div className="right-side">
+                    <p>explore works <i className="fa-solid fa-arrow-right-long"></i></p>
+                </div>
+            </div>
+
+    </div>
+  )
+}
+
+export default Introduction
